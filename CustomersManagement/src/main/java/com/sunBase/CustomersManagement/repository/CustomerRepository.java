4e47fa01507f6e54a13816edf366acc8e58a071d
radhikesh <radhikesh310@gmail.com>
2024-09-13 20:00:09 +0530

    Re-Updated

--- CustomersManagement/src/main/java/com/sunBase/CustomersManagement/repository/CustomerRepository.java
+++ CustomersManagement/src/main/java/com/sunBase/CustomersManagement/repository/CustomerRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.sunBase.CustomersManagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,19 +10,6 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-//
-//public interface CustomerRepository extends JpaRepository<Customer, String> {
-//	
-//    @Query("SELECT c FROM Customer c WHERE LOWER(c.first_name) LIKE LOWER(CONCAT('%', :first_name, '%'))")
-//    Page<Customer> searchByFirstName(@Param("first_name") String first_name, Pageable pageable);
-//    Page<Customer> findByEmailContainingIgnoreCase(String email, Pageable pageable);
-//    Page<Customer> findByPhoneContainingIgnoreCase(String phone, Pageable pageable);
-//    Page<Customer> findByCityContainingIgnoreCase(String city, Pageable pageable);
-//    boolean existsByEmail(String email);
-//    Optional<Customer> findByUuid(String uuid);
-//    boolean existsByUuid(String uuid);
-//    void deleteByUuid(String uuid);
-//}
 
 public interface CustomerRepository extends JpaRepository<Customer, String> {
 
@@ -38,44 +24,3 @@ public interface CustomerRepository extends JpaRepository<Customer, String> {
     boolean existsByUuid(String uuid);
     void deleteByUuid(String uuid);
 }
-=======
-package com.sunBase.CustomersManagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import com.sunBase.CustomersManagement.model.Customer;
-
-import java.util.Optional;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-//
-//public interface CustomerRepository extends JpaRepository<Customer, String> {
-//	
-//    @Query("SELECT c FROM Customer c WHERE LOWER(c.first_name) LIKE LOWER(CONCAT('%', :first_name, '%'))")
-//    Page<Customer> searchByFirstName(@Param("first_name") String first_name, Pageable pageable);
-//    Page<Customer> findByEmailContainingIgnoreCase(String email, Pageable pageable);
-//    Page<Customer> findByPhoneContainingIgnoreCase(String phone, Pageable pageable);
-//    Page<Customer> findByCityContainingIgnoreCase(String city, Pageable pageable);
-//    boolean existsByEmail(String email);
-//    Optional<Customer> findByUuid(String uuid);
-//    boolean existsByUuid(String uuid);
-//    void deleteByUuid(String uuid);
-//}
-
-public interface CustomerRepository extends JpaRepository<Customer, String> {
-
-	@Query("SELECT c FROM Customer c WHERE LOWER(c.first_name) LIKE LOWER(CONCAT('%', :first_name, '%'))")
-	Page<Customer> searchByFirstName(@Param("first_name") String first_name, Pageable pageable);
-
-    Page<Customer> findByEmailContainingIgnoreCase(String email, Pageable pageable);
-    Page<Customer> findByPhoneContainingIgnoreCase(String phone, Pageable pageable);
-    Page<Customer> findByCityContainingIgnoreCase(String city, Pageable pageable);
-    boolean existsByEmail(String email);
-    Optional<Customer> findByUuid(String uuid);
-    boolean existsByUuid(String uuid);
-    void deleteByUuid(String uuid);
-}
->>>>>>> 8532374ded0a8be6e32fcf0c01e928a81baf522d
